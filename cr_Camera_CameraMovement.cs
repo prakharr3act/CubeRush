0b2c309d07bd3da52bfbb7d96072da4ebcefe506
Prakhar Patel <prakhar.codes433@gmail.com>
2026-04-07 18:27:23 +0530

    Make camera follow player position

--- cr_Camera_CameraMovement.cs
+++ cr_Camera_CameraMovement.cs
@@ -1,11 +1,11 @@
-
 using UnityEngine;
 
 public class followPlayer : MonoBehaviour
 {
 
+    public Transform player;
     void Update()
     {
-        
+        transform.position = player.position;
     }
 }
